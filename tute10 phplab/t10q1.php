5934ca5b87aca1099dd5582b5cf575d392c93234
Dilmi01 <118357973+Dilmi01@users.noreply.github.com>
2023-07-11 14:38:07 +0530

    Add files via upload

---
+++ tute10 phplab/t10q1.php
@@ -0,0 +1,21 @@
+<html><head>
+<title>hellophp</title></head>
+<body>
+
+<?php
+$x=6;
+$y=4;
+$sum = $x +$y;
+$diff = $x -$y;
+$div = $x /$y;
+$mul = $x *$y;
+
+echo "Sum=".$sum."<br>";
+echo "Difference=".$diff."<br>";
+echo "Division=".$div."<br>";
+echo "Multiplication=".$mul."<br>";
+    
+?> 
+
+</body>
+</html>
